d02c3bfdbecaae2e91c5996b313d8f4da3efe4ba
Wynn @ Mon Aung <mon.aung@gmail.com>
2024-11-12 18:34:11 +1300

    Added Ordering microservice

---
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -0,0 +1,16 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+//add services to the container
+builder.Services.AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
+
+var app = builder.Build();
+
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
