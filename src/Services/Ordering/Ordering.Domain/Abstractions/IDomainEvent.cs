7b50ff7889d6bd75304cf3d328ba52277e8ecfe2
Wynn @ Mon Aung <mon.aung@gmail.com>
2024-11-14 03:01:06 +1300

    Added Entities, Value Objects, and Aggregate in Order Model

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Ordering.Domain.Abstractions
+{
+    public interface IDomainEvent:INotification //to alert MediatR event dispatch
+    {
+        Guid EventId => Guid.NewGuid();
+        public DateTime OccurredOn => DateTime.Now;
+        public string EventType => GetType().AssemblyQualifiedName!;
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderId.cs
@@ -0,0 +1,29 @@
+﻿using Ordering.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.ValueObjects
+{
+    public record OrderId
+    {
+        public Guid Value { get; }
+        public OrderId(Guid value)
+        {
+            Value = value;
+        }
+
+        public static OrderId Of(Guid value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            if (value == Guid.Empty)
+            {
+                throw new DomainException("OrderId cannot be empty.");
+            }
+
+            return new OrderId(value);
+        }
+    }
+}
